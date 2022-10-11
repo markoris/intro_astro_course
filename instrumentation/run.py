b948c61aaa404e6c047b6f5b89745cbacc235d0b
marko <mr3364@rit.edu>
2022-10-11 13:39:40 -0400

    minor

--- instrumentation/run.py
+++ instrumentation/run.py
@@ -2,6 +2,6 @@ import image as img
 
 image = img.Image(1024)
 image.add_objects(25)
-image.add_effects(["telescope_cover", "dark_current"])
+image.add_effects([])
 image.plot()
 image.write_fits("my_image.fits")
