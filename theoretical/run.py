d90ddd8601287ebac9dd24f0b9a90d5d5c34f8cc
marko <mr3364@rit.edu>
2022-10-26 12:57:33 -0400

    run.py: switch to 'nm' units

--- theoretical/run.py
+++ theoretical/run.py
@@ -1,7 +1,7 @@
 import numpy as np
 import star
 
-wavelength = np.linspace(1, 50, 500) * 10**-7
+wavelength = np.linspace(100, 2000, 500) * 10**-9 # nanometers
 
 star1 = star.Star('K')
 star2 = star.Star('A')
