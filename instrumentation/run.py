571dfec61362a8010185a11f4490a9e08185398b
Marko Ristic <mr3364@rit.edu>
2023-03-09 07:40:27 -0500

    temporary edits, run.py script will be removed when jupyter notebook is finished

--- instrumentation/run.py
+++ instrumentation/run.py
@@ -1,7 +1,13 @@
 import image as img
 
 image = img.Image(1024)
+#image = img.Image(256)
 image.add_objects(25)
-image.add_effects([])
+#image.add_effects([])
+#image.add_effects(["dark_current"])
+#image.add_effects(["telescope_cover"])
+image.add_effects(["dead_pixels"])
+#image.add_effects(["dead_arrays"])
+#image.add_effects(["dark_current", "telescope_cover"])
 image.plot()
 image.write_fits("my_image.fits")
