35a4fda6651935c20f08e9d16f5be93fbd24b9cb
kjtwagner <47577724+kjtwagner@users.noreply.github.com>
2023-03-16 10:46:30 -0500

    create obs jupyter notebook

--- observational/run.py
+++ observational/run.py
@@ -1,13 +1,23 @@
 import lightcurve as lc
 
-known_period_108 = 2.5255917769                                                                                              
-known_period_105 = 9.27358173
+# Kepler-695b: 5651104
 known_period_565 = 3.040330459
 
 data = lc.load_data('5651104/plot.tbl')
 time = data[0]
 flux = data[1]
-lc.plot_lc(time[0:1500],flux[0:1500])
+lc.plot_lc(time[0:500],flux[0:500])
+
+t_fold = lc.fold(time[0:500], known_period_565)
+lc.plot_phase(t_fold, flux[0:500])
+exit(0)
+# Kepler-18c: 8644288
+known_period_864 = 7.641567589 #14.8589085
+
+data = lc.load_data('8644288/plot.tbl')
+time = data[0]
+flux = data[1]
+lc.plot_lc(time[500:1000],flux[500:1000])
 
 t_fold = lc.fold(time[0:5000], known_period_565)
 lc.plot_phase(t_fold, flux[0:5000])
