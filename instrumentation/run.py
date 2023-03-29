b02fe7ae0a85cc9871a7947681c94a26994aecb4
Marko Ristic <mr3364@rit.edu>
2023-03-29 13:52:41 -0400

    adding probability of star/galaxy argument

--- instrumentation/run.py
+++ instrumentation/run.py
@@ -2,11 +2,11 @@ import image as img
 
 image = img.Image(1024)
 #image = img.Image(256)
-image.add_objects(25)
+image.add_objects(40, prob=0.3)
 #image.add_effects([])
 #image.add_effects(["dark_current"])
 #image.add_effects(["telescope_cover"])
-image.add_effects(["dead_pixels"])
+#image.add_effects(["dead_pixels"])
 #image.add_effects(["dead_arrays"])
 #image.add_effects(["dark_current", "telescope_cover"])
 image.plot()
